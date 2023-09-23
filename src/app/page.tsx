08855351963959dffb4989181259fc4ec7ba4b4a
d3iner <deinerdavid9@gmail.com>
2023-09-23 11:38:12 -0500

    commit
    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,139 @@
+'use client'
+import React, {useState, useCallback, useEffect} from 'react';
 import Image from 'next/image'
 
+interface Book{
+  id: number;
+  name: string;
+  author: string;
+  publishDate: string;
+  state:  'Prestado' | 'Dañado' | 'Perdido' | 'Disponible'
+}
+
 export default function Home() {
+
+  const [name, setName] = useState<string>('')
+  const [author, setAuthor] = useState<string>('')
+  const [publishDate, setPublishDate] = useState<string>('')
+  const [state, setState] = useState<string>('Prestado')
+  const [bookList, setBookList] = useState<Book[]>([])
+
+  useEffect(()=>{
+    console.log(bookList)
+  }, [bookList])
+
+  const clear = useCallback(()=>{
+    setName('')
+    setAuthor('')
+    setPublishDate('')
+  }, [])
+
+  const isValid = useCallback( (book:Book): boolean=>{
+    if (book.name.trim() === '') {
+      alert('Hay un error con el nombre ingresado, revise he intente denuevo')
+      return false
+    }
+    if (book.author.trim() === '') {
+      alert('Hay un error con el autor ingresado, revise he intente denuevo')
+      return false
+    }
+    if (book.publishDate.trim() === '') {
+      alert('Hay un error con la fecha, revise he intente denuevo')
+      return false
+    }
+    if (book.state.trim() === '') {
+      alert('Hay un error con el estado ingresado, revise he intente denuevo')
+      return false
+    }
+    return true
+  },[])
+
+  const onSubmit: React.FormEventHandler = useCallback( e =>{
+    e.preventDefault()
+    const book: Book = {
+      id: bookList[bookList.length - 1] ? bookList[bookList.length - 1]?.id + 1 : 1,
+      name: name,
+      author: author,
+      publishDate: publishDate,
+      state: state as  'Prestado' | 'Dañado' | 'Perdido' | 'Disponible'
+    }
+    console.log(book)
+    if (isValid(book)) {
+      setBookList([...bookList, book])
+      clear()
+    }
+  }, [name, author, publishDate, state, bookList])
+
+  const deleteBook = useCallback((id: number)=>{
+    console.log(id)
+    const newBookList = bookList.filter(book => book.id !== id)
+    console.log(newBookList)
+    setBookList(newBookList)
+  }, [bookList])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <form className='' onSubmit={onSubmit}>
+        <div className='grid gap-6 mb-6 md:grid-cols-2'>
+          <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre del libro</label>
+              <input type="text" value={name} onChange={e => setName(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          </div>
+          <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Autor</label>
+              <input type="text" value={author} onChange={e => setAuthor(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          </div>
+          <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Publicacion</label>
+              <input type="date" value={publishDate} onChange={e => setPublishDate(e.target.value)}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+          </div>
+          <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Estado</label>
+              <select defaultValue={state} value={state} onChange={e => setState(e.target.value )}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+              >
+                <option value="Prestado">Prestado</option>
+                <option value="Dañado">Dañado</option>
+                <option value="Perdido">Perdido</option>
+                <option value="Disponible">Disponible</option>
+              </select>
+          </div>
+          <button type='button' onClick={()=> clear()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded">
+            Clear
+          </button>
+          <button  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+            Submit
+          </button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </form>
+      <table className="w-full">
+          <thead>
+            <tr>
+              <th>Nombre del libro</th>
+              <th>Autor</th>
+              <th>Publicacion</th>
+              <th>Estado</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookList.map((book, index)=>[
+              <tr key={index}>
+                <td>{book.name}</td>
+                <td>{book.author}</td>
+                <td>{book.publishDate}</td>
+                <td>{book.state}</td>
+                <td><button type="button" 
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-red-700 rounded"
+                onClick={()=> deleteBook(book.id)}
+                >Eliminar</button></td>
+              </tr>
+            ])}
+          </tbody>
+        </table>
     </main>
   )
 }
